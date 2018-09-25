80f509252594d7bbd0b37cb83658830714e754b3
Hunter Freyer <yt@hjfreyer.com>
2018-09-24 20:56:13 -0400

    Add core game logic.

---
+++ src/gameplay.ts
@@ -0,0 +1,85 @@
+
+import * as status from './status';
+import produce from "immer";
+import * as immer from "immer";
+
+export type Game = Readonly<{
+  permutation: number[];
+  responses: string[][];
+}>;
+
+export function newGame(permutation: number[]): Game {
+  return {
+    permutation,
+    responses: permutation.map(_ => []),
+  };
+}
+
+function nextPlayer(permutation: number[], player: number): number {
+  const idx = permutation.indexOf(player);
+  return permutation[(idx + 1) % permutation.length];
+}
+
+function _submit(g: immer.DraftObject<Game>, player: number, word: string): Game {
+  const minLength = Math.min(...g.responses.map(a => a.length));
+
+  if (g.responses[player].length != minLength) {
+    throw status.alreadyExists();
+  }
+  if (minLength == g.permutation.length) {
+    // Game over.
+    throw status.preconditionFailed();
+  }
+
+  g.responses[player].push(word);
+  return g;
+}
+
+export function submit(g: Game, player: number, word: string): [status.Status, Game] {
+  try {
+    const g2 = produce(g, g => _submit(g, player, word));
+    return [status.ok(), g2];
+  } catch (s) {
+    return [s as status.Status, g];
+  }
+
+  // //  const [s, g2] = produce([status.ok(), g], ([s, g]: immer.Draft<[status.Status, Game]>) => _submit(g, player, word));
+  // return (g: Game) => {
+  //   if (!status.ok()) {
+  //7
+  //   }
+  // });
+}
+
+export type PlayerView = SimpleView | RespondToPromptView;
+
+type SimpleView = {
+  state: 'FIRST_PROMPT' | 'WAITING_FOR_OTHERS' | 'GAME_OVER';
+}
+
+type RespondToPromptView = {
+  state: 'RESPOND_TO_PROMPT';
+  prompt: string;
+}
+
+export function project(g: Game, player: number): PlayerView {
+  const minLength = Math.min(...g.responses.map(a => a.length));
+
+  if (minLength == g.permutation.length) {
+    return { state: 'GAME_OVER' };
+  }
+
+  if (minLength < g.responses[player].length) {
+    return { state: 'WAITING_FOR_OTHERS' };
+  }
+
+  if (minLength == 0) {
+    return { state: 'FIRST_PROMPT' };
+  }
+
+  const prompt = g.responses[nextPlayer(g.permutation, player)][minLength - 1];
+  return {
+    state: 'RESPOND_TO_PROMPT',
+    prompt
+  };
+}
---
+++ test/gameplay.test.ts
@@ -0,0 +1,81 @@
+
+import * as gp from '../src/gameplay';
+import * as status from '../src/status';
+
+export type Functor = (g: gp.Game) => [status.Status, gp.Game];
+
+const submit = (player: number, word: string): Functor => (g: gp.Game) => gp.submit(g, player, word);
+
+function snapshotTest(permutation: number[], actions: Functor[]): () => void {
+  return () => {
+    let g = gp.newGame(permutation);
+    expect(g).toMatchSnapshot();
+    for (const action of actions) {
+      let s: status.Status;
+      console.log(action(g));
+      [s, g] = action(g);
+      expect(s).toEqual(status.ok());
+      const views = g.permutation.map((_, idx) => gp.project(g, idx));
+      expect({ game: g, views }).toMatchSnapshot();
+    }
+  };
+}
+
+function errorTest(err: status.Status, permutation: number[], actions: Functor[]): () => void {
+  return () => {
+    let g = gp.newGame(permutation);
+    for (const action of actions.slice(0, actions.length - 1)) {
+      let s: status.Status;
+      [s, g] = action(g);
+      expect(s).toEqual(status.ok());
+    }
+    let [s, g2] = actions[actions.length - 1](g);
+    expect(s).toEqual(err);
+    expect(g2).toEqual(g);
+  };
+}
+
+test('new game', snapshotTest([2, 0, 1], []));
+
+test('double submit', errorTest(status.alreadyExists(), [2, 0, 1], [
+  submit(1, 'foo'),
+  submit(1, 'bar'),
+]));
+
+test('do a round', snapshotTest([2, 0, 1], [
+  submit(0, 'foo'),
+  submit(2, 'bar'),
+  submit(1, 'baz'),
+]));
+
+test('do a game', snapshotTest([2, 0, 1], [
+  submit(0, 'one'),
+  submit(1, 'ay'),
+  submit(2, 'do'),
+  submit(1, 're'),
+  submit(0, 'bee'),
+  submit(2, 'two'),
+  submit(1, 'three'),
+  submit(2, 'cee'),
+  submit(0, 'mi'),
+]));
+
+test('permutation is always fully cyclic', snapshotTest([0, 2, 1], [
+  submit(0, 'ay'),
+  submit(1, 'do'),
+  submit(2, 'one'),
+  submit(2, 're'),
+  submit(0, 'two'),
+  submit(1, 'bee'),
+  submit(1, 'three'),
+  submit(2, 'cee'),
+  submit(0, 'mi'),
+]));
+
+test('play past end', errorTest(status.preconditionFailed(), [0, 1], [
+  submit(0, 'foo'),
+  submit(1, 'bar'),
+  submit(0, 'baz'),
+  submit(1, 'qux'),
+  submit(0, 'bad'),
+]));
