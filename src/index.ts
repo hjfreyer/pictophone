cb6b1a926f43d6161749c4f0aada42812ed990f6
Hunter Freyer <yt@hjfreyer.com>
2018-09-29 17:02:51 -0400

    Make it buildable as JS.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import * as base from './framework/base';
+import * as local from './framework/localstorage';
+import * as actions from './actions';
+import * as model from './model';
+import * as streams from './streams';
+
+export { base, local, actions, model, streams };
