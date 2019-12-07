4b4bf3b585919f68c274c84fc1037131cdd2ebbd
Hunter Freyer <yt@hjfreyer.com>
2019-12-07 16:35:41 -0500

    Add new build process for cloudbuild and firebase hosting.

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,48 @@ export type Config = {
     storageBucket: string
 }
 
+type Environment = 'production' | 'qa' | 'test' | 'development'
+
+export function getEnvironment(): Environment {
+    switch (process.env.NODE_ENV) {
+        case 'development':
+        case 'test':
+            return process.env.NODE_ENV
+        case 'production':
+            switch (process.env.REACT_APP_ENV) {
+                case 'production':
+                case 'qa':
+                    return process.env.REACT_APP_ENV
+                default:
+                    throw new Error('undefined REACT_APP_ENV: ' + process.env.REACT_APP_ENV)
+            }
+    }
+}
+
+const DEV_CONFIG: Config = {
+    backendAddr: 'http://localhost:3000',
+    storageBucket: 'pictophone-app-drawings',
+}
+
+const PROD_CONFIG: Config = {
+    backendAddr: 'https://api.pictophone.app',
+    storageBucket: 'pictophone-app-drawings',
+}
+
+const QA_CONFIG: Config = {
+    backendAddr: 'https://api.pictophone.app',
+    storageBucket: 'pictophone-app-drawings',
+}
+
 export default function Config(): Config {
-    return {
-        backendAddr: process.env.REACT_APP_BACKEND!,
-        storageBucket: 'pictophone-app-drawings',
+    switch (getEnvironment()) {
+        case 'development':
+            return DEV_CONFIG
+        case 'production':
+            return PROD_CONFIG
+        case 'qa':
+            return QA_CONFIG
+        default:
+            throw new Error('unsupported')
     }
 }
