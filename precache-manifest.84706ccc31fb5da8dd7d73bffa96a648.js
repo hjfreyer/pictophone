9334a7850bd9ab23479f7d39af41c7f51d77643d
Hunter Freyer <yt@hjfreyer.com>
2021-01-31 22:55:10 +0000

    Updates

--- precache-manifest.84706ccc31fb5da8dd7d73bffa96a648.js
+++ precache-manifest.84706ccc31fb5da8dd7d73bffa96a648.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e00ebe1b974f736376fda46b4b29b69f",
+    "revision": "e91a486ca7789ce3bb913bbf6f6c90ae",
     "url": "/index.html"
   },
   {
-    "revision": "fd0d9c4b04071be227cd",
-    "url": "/static/css/main.171912ec.chunk.css"
+    "revision": "2da479d8cfe13341f68f",
+    "url": "/static/css/main.2e1f1c79.chunk.css"
   },
   {
     "revision": "0a9fadbf99ee94a1852d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.88e566bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd0d9c4b04071be227cd",
-    "url": "/static/js/main.25f21daf.chunk.js"
+    "revision": "2da479d8cfe13341f68f",
+    "url": "/static/js/main.c0879a0f.chunk.js"
   },
   {
     "revision": "a50282816c55998ef4f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a44ed8d4eb58762dc95e9d1594bf8a8b.js"
+  "/precache-manifest.84706ccc31fb5da8dd7d73bffa96a648.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
